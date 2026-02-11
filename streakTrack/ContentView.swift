158bbaaafcac08d6e5079b21531d6e2a797d53d5
Your Name <you@example.com>
2026-02-11 23:31:21 +0530

    Initial Commit

---
+++ streakTrack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  streakTrack
+//
+//  Created by himanshu yadav on 22/11/1947 Saka.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ streakTrack/streakTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  streakTrackApp.swift
+//  streakTrack
+//
+//  Created by himanshu yadav on 22/11/1947 Saka.
+//
+
+import SwiftUI
+
+@main
+struct streakTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
